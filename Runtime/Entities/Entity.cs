3327ce43016063d0fbf841fdb4ad8b4caaa769c4
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-01-19 06:30:52 +0300

    docs: add entity documentation

--- Runtime/Entities/Entity.cs
+++ Runtime/Entities/Entity.cs
@@ -2,17 +2,44 @@
 
 namespace ECS
 {
+    /// <summary>
+    /// Represents entity in Entity-Component-System architecture.
+    /// Consists of identifier <see cref="Id"/> and generation number <see cref="Gen"/> -
+    /// this pair of values is unique for each entity.<br/>
+    /// <br/>
+    /// <i>Entities are kind of game objects with associated data and no logic.</i>
+    /// </summary>
     public readonly struct Entity : IEquatable<Entity>
     {
+        /// <summary>
+        /// Unique identifier of entity.
+        /// There can not be more than one existing entity with the same identifier.
+        /// To be able to reuse identifier when entity is destroyed, generation number <see cref="Gen"/> is used.
+        /// </summary>
         public readonly int Id;
+
+        /// <summary>
+        /// Generation number of entity.
+        /// Each time entity is destroyed, its identifier <see cref="Id"/> might be reused.
+        /// But to preserve unique definition of entity, its generation number is incremented.
+        /// </summary>
         public readonly int Gen;
 
+        /// <param name="id">Identifier</param>
+        /// <param name="gen">Generation number</param>
         public Entity(int id, int gen)
         {
             Id = id;
             Gen = gen;
         }
 
+        /// <summary>
+        /// NULL-entity is an entity with identifier <see cref="Id"/> = 0, no matter its generation number <see cref="Gen"/>.<br/>
+        /// <br/>
+        /// <i>Always use this method to check entity for NULL,
+        /// because '==' and 'Equals' check the equality of generation numbers.</i>
+        /// </summary>
+        /// <returns>True if entity is a NULL-entity, false otherwise</returns>
         public bool IsNull()
         {
             return Id == 0;
