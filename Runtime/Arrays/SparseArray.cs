1838848a33b600faa35e28bbb40db2c6e477b72e
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-01-19 10:37:08 +0300

    feat: add sparse array

---
+++ Runtime/Arrays/SparseArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ECS
+{
+    public sealed class SparseArray<T>
+    {
+        private const int DefaultCapacity = 64;
+
+        private T[] _array;
+
+        public int Capacity { get; private set; }
+
+        public T this[int index]
+        {
+            get => _array[index];
+            set
+            {
+                if (Capacity <= index)
+                {
+                    ResizeForIndex(index);
+                }
+
+                _array[index] = value;
+            }
+        }
+
+        public SparseArray(int capacity = DefaultCapacity)
+        {
+            _array = new T[capacity];
+            Capacity = capacity;
+        }
+
+        public ReadOnlySpan<T> AsReadOnlySpan()
+        {
+            return _array.AsSpan();
+        }
+
+        private void ResizeForIndex(int index)
+        {
+            do
+            {
+                Capacity *= 2;
+            } while (index >= Capacity);
+
+            Array.Resize(ref _array, Capacity);
+        }
+    }
+}
