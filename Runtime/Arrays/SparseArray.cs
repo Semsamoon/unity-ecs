969809f3c69b7b31cc17b53acaccef4eaa68e692
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-02 22:59:29 +0300

    docs: reduce sparse array documentation

--- Runtime/Arrays/SparseArray.cs
+++ Runtime/Arrays/SparseArray.cs
@@ -4,31 +4,16 @@ using System.Collections.Generic;
 namespace ECS
 {
     /// <summary>
-    /// Represents sparse array.
-    /// Consists of internal array that is extended automatically.<br/>
-    /// <br/>
-    /// <i>Sparse arrays are like standard arrays, but with automatic extension.
-    /// In most cases they store indexes for <see cref="DenseArray{T}"/>,
-    /// and indexes for them are <see cref="Entity"/> <see cref="Entity.Id"/>.</i>
+    /// Sparse array is an <see cref="Array"/> with automatic extension.
     /// </summary>
-    /// <typeparam name="T">Type of internal array</typeparam>
     public sealed class SparseArray<T>
     {
         private const int DefaultLength = 64;
 
         private T[] _array;
 
-        /// <summary>
-        /// Current length of internal array.
-        /// Initialized in constructor and can not be increased manually.
-        /// </summary>
         public int Length => _array.Length;
 
-        /// <summary>
-        /// Access to internal array.
-        /// Extends internal array if <paramref name="index"/> is greater than or equals to array's <see cref="Length"/>.
-        /// </summary>
-        /// <param name="index">Index of array element</param>
         public ref T this[int index]
         {
             get
@@ -43,7 +28,6 @@ namespace ECS
             _array = new T[DefaultLength];
         }
 
-        /// <param name="length">Initial <see cref="Length"/> of internal array</param>
         public SparseArray(int length)
         {
             length = Math.Max(length, 2);
@@ -60,11 +44,6 @@ namespace ECS
             return (IEnumerator<T>)_array.GetEnumerator();
         }
 
-        /// <summary>
-        /// Extends internal array to the specified <paramref name="index"/>.
-        /// <see cref="Length"/> is increased 2 times until <paramref name="index"/> can be accessed.
-        /// </summary>
-        /// <param name="index">Index that needs to be accessed</param>
         private void ExtendTo(int index)
         {
             var length = Length;
