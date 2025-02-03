0256e8a8b9f2acd6b85e6afd09247f2aa6484989
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-03 18:12:06 +0300

    test: update pool tests

--- Tests/Editor/Pools/PoolTests.cs
+++ Tests/Editor/Pools/PoolTests.cs
@@ -7,83 +7,83 @@ namespace ECS.Tests
         [Test]
         public void ConstructorSetsValidValues()
         {
-            Assert.AreEqual(1, new Pool(10, 10).Length);
-            Assert.DoesNotThrow(() => new Pool(-10, -10));
+            Assert.AreEqual(10, new Pool(10, 10).Capacity);
+            Assert.Positive(new Pool(-10, -10).Capacity);
+            Assert.AreEqual(0, new Pool().Length);
 
-            Assert.AreEqual(1, new Pool<int>(10, 10).Length);
-            Assert.DoesNotThrow(() => new Pool<int>(-10, -10));
-
-            // Initial length must be 1
-            Assert.AreEqual(1, new Pool().Length);
-            Assert.AreEqual(1, new Pool<int>().Length);
+            Assert.AreEqual(10, new Pool<int>(10, 10).Capacity);
+            Assert.Positive(new Pool<int>(-10, -10).Capacity);
+            Assert.AreEqual(0, new Pool<int>().Length);
         }
 
         [Test]
         public void GettersAreCorrect()
         {
-            var pool1 = new Pool(10, 10);
+            var pool1 = new Pool(2, 2);
             pool1.Add(new Entity(1, 0));
-            Assert.AreEqual(new Entity(1, 0), pool1[1]);
+            Assert.AreEqual(new Entity(1, 0), pool1[0]);
 
-            var pool2 = new Pool<int>(4, 4);
-            pool2.AddOrSet(new Entity(1, 0), 10);
-            Assert.AreEqual(new Entity(1, 0), pool2[1]);
+            var pool2 = new Pool<int>(2, 2);
+            pool2.Set(new Entity(1, 0), 10);
+            Assert.AreEqual(new Entity(1, 0), pool2[0].Entity);
 
-            pool2.Get(1) = 20;
-            Assert.AreEqual(20, pool2.Get(1));
+            pool2.Get(0) = 20;
+            Assert.AreEqual(20, pool2.Get(0));
 
             pool2.Get(new Entity(1, 0)) = 30;
-            Assert.AreEqual(30, pool2.Get(1));
+            Assert.AreEqual(30, pool2.Get(new Entity(1, 0)));
         }
 
         [Test]
         public void AddingIsCorrect()
         {
-            var pool1 = new Pool(4, 4);
+            var pool1 = new Pool(2, 2);
             pool1.Add(new Entity(1, 0));
 
-            Assert.AreEqual(2, pool1.Length);
-            Assert.AreEqual(new Entity(1, 0), pool1[1]);
+            Assert.AreEqual(1, pool1.Length);
+            Assert.AreEqual(new Entity(1, 0), pool1[0]);
 
-            var pool2 = new Pool<int>(4, 4);
-            pool2.AddOrSet(new Entity(1, 0), 10);
+            var pool2 = new Pool<int>(2, 2);
+            pool2.Set(new Entity(1, 0), 10);
 
-            Assert.AreEqual(2, pool2.Length);
-            Assert.AreEqual(10, pool2.Get(1));
+            Assert.AreEqual(1, pool2.Length);
+            Assert.AreEqual(10, pool2.Get(0));
         }
 
         [Test]
         public void SettingIsCorrect()
         {
-            var pool1 = new Pool(4, 4);
+            var pool1 = new Pool(2, 2);
             pool1.Add(new Entity(1, 0));
-            pool1.Add(new Entity(1, 0));
+            Assert.DoesNotThrow(() => pool1.Add(new Entity(1, 0)));
 
-            Assert.AreEqual(2, pool1.Length);
-            Assert.AreEqual(new Entity(1, 0), pool1[1]);
+            Assert.AreEqual(1, pool1.Length);
+            Assert.AreEqual(new Entity(1, 0), pool1[0]);
 
-            var pool2 = new Pool<int>(4, 4);
-            pool2.AddOrSet(new Entity(1, 0), 10);
-            pool2.AddOrSet(new Entity(1, 0), 20);
+            var pool2 = new Pool<int>(2, 2);
+            pool2.Set(new Entity(1, 0), 10);
+            Assert.DoesNotThrow(() => pool2.Set(new Entity(1, 0), 20));
 
-            Assert.AreEqual(2, pool2.Length);
-            Assert.AreEqual(20, pool2.Get(1));
+            Assert.AreEqual(1, pool2.Length);
+            Assert.AreEqual(20, pool2.Get(0));
         }
 
         [Test]
         public void ContainsIsCorrect()
         {
-            var pool1 = new Pool(4, 4);
+            var pool1 = new Pool(2, 2);
             pool1.Add(new Entity(1, 0));
 
             Assert.True(pool1.Contains(new Entity(1, 0)));
             Assert.False(pool1.Contains(new Entity(2, 0)));
+            Assert.False(pool1.Contains(new Entity()));
 
-            var pool2 = new Pool<int>(4, 4);
-            pool2.AddOrSet(new Entity(1, 0), 10);
+            var pool2 = new Pool<int>(2, 2);
+            pool2.Set(new Entity(1, 0), 10);
 
             Assert.True(pool2.Contains(new Entity(1, 0)));
             Assert.False(pool2.Contains(new Entity(2, 0)));
+            Assert.False(pool2.Contains(new Entity()));
         }
 
         [Test]
@@ -94,8 +94,8 @@ namespace ECS.Tests
             Assert.DoesNotThrow(() => pool1.Add(new Entity(2, 0)));
 
             var pool2 = new Pool<int>(1, 1);
-            Assert.DoesNotThrow(() => pool2.AddOrSet(new Entity(1, 0), 10));
-            Assert.DoesNotThrow(() => pool2.AddOrSet(new Entity(2, 0), 10));
+            Assert.DoesNotThrow(() => pool2.Set(new Entity(1, 0), 10));
+            Assert.DoesNotThrow(() => pool2.Set(new Entity(2, 0), 10));
         }
 
         [Test]
@@ -106,15 +106,15 @@ namespace ECS.Tests
 
             pool1.Add(new Entity(1, 0));
             Assert.DoesNotThrow(() => pool1.Remove(new Entity(1, 0)));
-            Assert.AreEqual(1, pool1.Length);
+            Assert.AreEqual(0, pool1.Length);
             Assert.DoesNotThrow(() => pool1.Contains(new Entity(1, 0)));
 
             var pool2 = new Pool<int>(2, 2);
             Assert.DoesNotThrow(() => pool2.Remove(new Entity(1, 0)));
 
-            pool2.AddOrSet(new Entity(1, 0), 10);
+            pool2.Set(new Entity(1, 0), 10);
             Assert.DoesNotThrow(() => pool2.Remove(new Entity(1, 0)));
-            Assert.AreEqual(1, pool2.Length);
+            Assert.AreEqual(0, pool2.Length);
             Assert.DoesNotThrow(() => pool2.Contains(new Entity(1, 0)));
         }
 
@@ -124,31 +124,67 @@ namespace ECS.Tests
             var pool1 = new Pool(4, 4);
             var pool2 = new Pool<int>(4, 4);
 
+            for (var i = 0; i < 3; i++)
+            {
+                pool1.Add(new Entity(i + 1, 0));
+                pool2.Set(new Entity(i + 1, 0), i);
+            }
+
             // Foreach-loop
+            var j = 0;
+
             foreach (var i in pool1.AsReadOnlySpan())
             {
-                Assert.AreEqual(new Entity(0, 0), i);
+                Assert.AreEqual(new Entity(j + 1, 0), i);
+                j++;
             }
+
+            j = 0;
 
             foreach (var i in pool2.AsReadOnlySpan())
             {
-                Assert.AreEqual((new Entity(0, 0), 0), i);
-            }
-
-            for (var i = 1; i < 3; i++)
-            {
-                pool1.Add(new Entity(i, i));
-                pool2.AddOrSet(new Entity(i, i), i);
+                Assert.AreEqual((new Entity(j + 1, 0), j), i);
+                j++;
             }
 
             var span1 = pool1.AsReadOnlySpan();
             var span2 = pool2.AsReadOnlySpan();
 
             // For-loop
-            for (var i = 1; i < 3; i++)
+            for (var i = 0; i < 3; i++)
             {
-                Assert.AreEqual(new Entity(i, i), span1[i]);
-                Assert.AreEqual((new Entity(i, i), i), span2[i]);
+                Assert.AreEqual(new Entity(i + 1, 0), span1[i]);
+                Assert.AreEqual((new Entity(i + 1, 0), i), span2[i]);
+            }
+        }
+
+        [Test]
+        public void EnumerableIsCorrect()
+        {
+            var pool1 = new Pool(4, 4);
+            pool1.Add(new Entity(1, 0));
+            pool1.Add(new Entity(2, 0));
+            pool1.Add(new Entity(3, 0));
+
+            var j = 0;
+
+            foreach (var i in pool1)
+            {
+                j++;
+                Assert.AreEqual(new Entity(j, 0), i);
+            }
+
+            var pool2 = new Pool<int>(4, 4);
+            pool2.Set(new Entity(1, 0), 10);
+            pool2.Set(new Entity(2, 0), 20);
+            pool2.Set(new Entity(3, 0), 30);
+
+            j = 0;
+
+            foreach (var i in pool2)
+            {
+                j++;
+                Assert.AreEqual((new Entity(j, 0), j * 10), i);
             }
         }
     }
