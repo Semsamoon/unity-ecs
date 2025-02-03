50944a9a240d220ee45780dc82e655f6552b2147
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-03 15:10:16 +0300

    test: update dense array tests

--- Tests/Editor/Arrays/DenseArrayTests.cs
+++ Tests/Editor/Arrays/DenseArrayTests.cs
@@ -12,84 +12,107 @@ namespace ECS.Tests
             Assert.Positive(new DenseArray<int>(0).Capacity);
             Assert.Positive(new DenseArray<int>().Capacity);
 
-            // Initial length must be 1
-            Assert.AreEqual(1, new DenseArray<int>().Length);
+            Assert.AreEqual(0, new DenseArray<int>().Length);
         }
 
         [Test]
         public void SetterAndGetterAreCorrect()
         {
-            var denseArray1 = new DenseArray<int>(10);
-            denseArray1.Add(10);
-            denseArray1[1] = 20;
-            Assert.AreEqual(20, denseArray1[1]);
+            var denseArray = new DenseArray<int>(2);
+            denseArray.Add(10);
+            denseArray[0] = 20;
+
+            Assert.AreEqual(20, denseArray[0]);
         }
 
         [Test]
         public void AddingIsCorrect()
         {
-            var denseArray1 = new DenseArray<int>(2);
-            denseArray1.Add(10);
+            var denseArray = new DenseArray<int>(2);
+            denseArray.Add(10);
 
-            Assert.AreEqual(2, denseArray1.Length);
-            Assert.AreEqual(10, denseArray1[1]);
+            Assert.AreEqual(1, denseArray.Length);
+            Assert.AreEqual(10, denseArray[0]);
         }
 
         [Test]
         public void CanAutomaticallyExtend()
         {
-            var denseArray1 = new DenseArray<int>(1);
-            Assert.AreEqual(1, denseArray1.Capacity);
+            var denseArray = new DenseArray<int>(2);
+            Assert.AreEqual(2, denseArray.Capacity);
 
-            denseArray1.Add(10);
-            Assert.AreEqual(2, denseArray1.Capacity);
+            denseArray.Add(10);
+            denseArray.Add(20);
+            Assert.AreEqual(2, denseArray.Capacity);
+
+            denseArray.Add(30);
+            Assert.AreEqual(4, denseArray.Capacity);
         }
 
         [Test]
         public void RemovingIsCorrect()
         {
-            var denseArray1 = new DenseArray<int>(2);
-            Assert.DoesNotThrow(() => denseArray1.RemoveAt(1));
+            var denseArray = new DenseArray<int>(2);
+            Assert.DoesNotThrow(() => denseArray.RemoveAt(0));
 
-            denseArray1.Add(10);
-            Assert.DoesNotThrow(() => denseArray1.RemoveAt(1));
-            Assert.AreEqual(1, denseArray1.Length);
+            denseArray.Add(10);
+            Assert.DoesNotThrow(() => denseArray.RemoveAt(0));
+            Assert.AreEqual(0, denseArray.Length);
         }
 
         [Test]
         public void RemovingUsesBackSwap()
         {
-            var denseArray1 = new DenseArray<int>(4);
-            denseArray1.Add(10);
-            denseArray1.Add(20);
-            denseArray1.Add(30);
+            var denseArray = new DenseArray<int>(4);
+            denseArray.Add(10);
+            denseArray.Add(20);
+            denseArray.Add(30);
 
-            denseArray1.RemoveAt(1);
-            Assert.AreEqual(30, denseArray1[1]);
+            denseArray.RemoveAt(0);
+            Assert.AreEqual(30, denseArray[0]);
         }
 
         [Test]
         public void ReadOnlySpanIsCorrect()
         {
-            var denseArray1 = new DenseArray<int>(4);
+            var denseArray = new DenseArray<int>(4);
+
+            for (var i = 0; i < 3; i++)
+            {
+                denseArray.Add(10);
+            }
 
             // Foreach-loop
-            foreach (var i in denseArray1.AsReadOnlySpan())
+            foreach (var i in denseArray.AsReadOnlySpan())
             {
-                Assert.AreEqual(0, i);
+                Assert.AreEqual(10, i);
             }
 
-            for (var i = 1; i < 3; i++)
+            for (var i = 0; i < 3; i++)
             {
-                denseArray1.Add(i);
+                denseArray[i] = i;
             }
 
-            var span1 = denseArray1.AsReadOnlySpan();
+            var span = denseArray.AsReadOnlySpan();
 
             // For-loop
-            for (var i = 1; i < 3; i++)
+            for (var i = 0; i < 3; i++)
             {
-                Assert.AreEqual(i, span1[i]);
+                Assert.AreEqual(i, span[i]);
+            }
+        }
+
+        [Test]
+        public void EnumerableIsCorrect()
+        {
+            var denseArray = new DenseArray<int>(4);
+            denseArray.Add(10);
+            denseArray.Add(10);
+            denseArray.Add(10);
+
+            foreach (var i in denseArray)
+            {
+                Assert.AreEqual(10, i);
             }
         }
     }
