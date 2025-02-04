88ae48df7989dfc52d06ec45557c6211d06eaea0
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-04 09:41:55 +0300

    docs: reduce entity documentation

--- Runtime/Entities/Entity.cs
+++ Runtime/Entities/Entity.cs
@@ -3,31 +3,15 @@
 namespace ECS
 {
     /// <summary>
-    /// Represents entity in Entity-Component-System architecture.
-    /// Consists of identifier <see cref="Id"/> and generation number <see cref="Gen"/> -
-    /// this pair of values is unique for each entity.<br/>
+    /// Entity in Entity-Component-System architecture.<br/>
     /// <br/>
-    /// <i>Entities are kind of game objects with associated data and no logic.
-    /// Entity with <see cref="Id"/> = 0 is invalid and called NULL-entity.</i>
+    /// <i>Entity with <see cref="Id"/> = 0 is a NULL-entity.</i>
     /// </summary>
     public readonly struct Entity : IEquatable<Entity>
     {
-        /// <summary>
-        /// Unique identifier of entity.
-        /// There can not be more than one existing entity with the same identifier.
-        /// To be able to reuse identifier when entity is destroyed, generation number <see cref="Gen"/> is used.
-        /// </summary>
         public readonly int Id;
-
-        /// <summary>
-        /// Generation number of entity.
-        /// Each time entity is destroyed, its identifier <see cref="Id"/> might be reused.
-        /// But to preserve unique definition of entity, its generation number is incremented.
-        /// </summary>
         public readonly int Gen;
 
-        /// <param name="id">Identifier</param>
-        /// <param name="gen">Generation number</param>
         public Entity(int id, int gen)
         {
             id = Math.Max(id, 0);
@@ -37,12 +21,8 @@ namespace ECS
         }
 
         /// <summary>
-        /// NULL-entity is an entity with identifier <see cref="Id"/> = 0, no matter its generation number <see cref="Gen"/>.<br/>
-        /// <br/>
-        /// <i>Always use this method to check entity for NULL,
-        /// because '==' and 'Equals' check the equality of generation numbers.</i>
+        /// <i>Use only this method to check for NULL-entity.</i>
         /// </summary>
-        /// <returns>True if entity is a NULL-entity, false otherwise</returns>
         public bool IsNull()
         {
             return Id == 0;
