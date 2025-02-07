c8ad4a62cd60393d56bf701cba162ee112c16e48
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-07 10:15:41 +0300

    feat: add world

---
+++ Runtime/Worlds/World.cs
@@ -0,0 +1,26 @@
+﻿namespace ECS
+{
+    public sealed class World
+    {
+        public Entities EntitiesInternal { get; }
+        public Pools PoolsInternal { get; }
+        public Filters FiltersInternal { get; }
+
+        public IEntities Entities => EntitiesInternal;
+        public IPools Pools => PoolsInternal;
+
+        public World()
+        {
+            EntitiesInternal = new Entities();
+            PoolsInternal = new Pools();
+            FiltersInternal = new Filters();
+        }
+
+        public World((int sparse, int dense) entitiesCapacity, int poolsCapacity, (int included, int excluded) filtersCapacity)
+        {
+            EntitiesInternal = new Entities(entitiesCapacity.sparse, entitiesCapacity.dense);
+            PoolsInternal = new Pools(poolsCapacity);
+            FiltersInternal = new Filters(filtersCapacity.included, filtersCapacity.excluded);
+        }
+    }
+}
