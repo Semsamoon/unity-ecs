576fc237b835ee889adebfcc925400156ddf6360
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-07 23:17:30 +0300

    feat: add systems to world

--- Runtime/Worlds/IWorld.cs
+++ Runtime/Worlds/IWorld.cs
@@ -5,5 +5,6 @@
         public IEntities Entities { get; }
         public IPools Pools { get; }
         public IFilters Filters { get; }
+        public ISystems Systems { get; }
     }
 }
--- Runtime/Worlds/World.cs
+++ Runtime/Worlds/World.cs
@@ -5,23 +5,27 @@
         public Entities EntitiesInternal { get; }
         public Pools PoolsInternal { get; }
         public Filters FiltersInternal { get; }
+        public Systems SystemsInternal { get; }
 
         public IEntities Entities => EntitiesInternal;
         public IPools Pools => PoolsInternal;
         public IFilters Filters => FiltersInternal;
+        public ISystems Systems => SystemsInternal;
 
         public World()
         {
             EntitiesInternal = new Entities();
             PoolsInternal = new Pools(this);
             FiltersInternal = new Filters(this);
+            SystemsInternal = new Systems(this);
         }
 
-        public World((int sparse, int dense) entitiesCapacity, int poolsCapacity, (int included, int excluded) filtersCapacity)
+        public World((int sparse, int dense) entitiesCapacity, int poolsCapacity, (int included, int excluded) filtersCapacity, int systemsCapacity)
         {
             EntitiesInternal = new Entities(entitiesCapacity.sparse, entitiesCapacity.dense);
             PoolsInternal = new Pools(this, poolsCapacity);
             FiltersInternal = new Filters(this, filtersCapacity.included, filtersCapacity.excluded);
+            SystemsInternal = new Systems(this, systemsCapacity);
         }
     }
 }
