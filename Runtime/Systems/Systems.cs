7d72051cc5af116ff4befbd862155ce7d285b517
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-07 23:14:12 +0300

    feat: add systems

---
+++ Runtime/Systems/Systems.cs
@@ -0,0 +1,51 @@
+﻿namespace ECS
+{
+    public sealed class Systems : ISystems
+    {
+        private readonly World _world;
+        private readonly DenseArray<ISystem> _systems;
+
+        public Systems(World world)
+        {
+            _world = world;
+            _systems = new DenseArray<ISystem>();
+        }
+
+        public Systems(World world, int capacity)
+        {
+            _world = world;
+            _systems = new DenseArray<ISystem>(capacity);
+        }
+
+        public ISystems Add<T>() where T : ISystem, new()
+        {
+            var system = new T();
+            _systems.Add(system);
+            system.Initialize(_world);
+            return this;
+        }
+
+        public ISystems Add(ISystem system)
+        {
+            _systems.Add(system);
+            system.Initialize(_world);
+            return this;
+        }
+
+        public void Update()
+        {
+            foreach (var system in _systems)
+            {
+                system.Update();
+            }
+        }
+
+        public void Destroy()
+        {
+            foreach (var system in _systems)
+            {
+                system.Destroy();
+            }
+        }
+    }
+}
