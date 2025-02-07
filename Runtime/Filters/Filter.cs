ab834079b7b737d9e10b3f7e97922ac110312324
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-07 11:24:09 +0300

    refactor: replace pool in filter with arrays

--- Runtime/Filters/Filter.cs
+++ Runtime/Filters/Filter.cs
@@ -10,27 +10,30 @@ namespace ECS
     public sealed class Filter : IFilter
     {
         private readonly SparseArray<int> _counts;
-        private readonly Pool _filtered;
+        private readonly SparseArray<int> _sparseArray;
+        private readonly DenseArray<Entity> _denseArray;
         private readonly int _sum;
 
-        public int Capacity => _filtered.Capacity;
-        public int Length => _filtered.Length;
+        public int Capacity => _denseArray.Capacity;
+        public int Length => _denseArray.Length;
 
-        public Entity this[int index] => _filtered[index];
+        public Entity this[int index] => _denseArray[index];
 
         public Filter(int sum)
         {
             sum = Math.Max(0, sum);
-            _filtered = new Pool(null, null);
             _counts = new SparseArray<int>();
+            _sparseArray = new SparseArray<int>();
+            _denseArray = new DenseArray<Entity>();
             _sum = sum;
         }
 
         public Filter(int sum, int sparseCapacity, int denseCapacity)
         {
             sum = Math.Max(0, sum);
-            _filtered = new Pool(null, null, sparseCapacity, denseCapacity);
             _counts = new SparseArray<int>(sparseCapacity);
+            _sparseArray = new SparseArray<int>(sparseCapacity);
+            _denseArray = new DenseArray<Entity>(denseCapacity);
             _sum = sum;
         }
 
@@ -38,30 +41,56 @@ namespace ECS
         {
             if (_counts[entity.Id] == _sum)
             {
-                _filtered.Remove(entity);
+                Remove(entity);
             }
 
             _counts[entity.Id] += difference;
 
             if (_counts[entity.Id] == _sum)
             {
-                _filtered.Add(entity);
+                Add(entity);
             }
         }
 
         public bool Contains(Entity entity)
         {
-            return _filtered.Contains(entity);
+            return !entity.IsNull() && _denseArray[_sparseArray[entity.Id]] == entity;
         }
 
         public ReadOnlySpan<Entity> AsReadOnlySpan()
         {
-            return _filtered.AsReadOnlySpan();
+            return _denseArray.AsReadOnlySpan();
         }
 
         public IEnumerator<Entity> GetEnumerator()
         {
-            return _filtered.GetEnumerator();
+            return _denseArray.GetEnumerator();
+        }
+
+        private void Add(Entity entity)
+        {
+            if (entity.IsNull() || _denseArray[_sparseArray[entity.Id]] == entity)
+            {
+                return;
+            }
+
+            _sparseArray[entity.Id] = Length;
+            _denseArray.Add(entity);
+        }
+
+        private void Remove(Entity entity)
+        {
+            var index = _sparseArray[entity.Id];
+
+            if (entity.IsNull() || _denseArray[index] != entity)
+            {
+                return;
+            }
+
+            _sparseArray[_denseArray[^1].Id] = index;
+            _sparseArray[entity.Id] = 0;
+            _denseArray[index] = new Entity();
+            _denseArray.RemoveAt(index);
         }
     }
 }
