1e3089b5b34aee0172baea15e035aa95418e274b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-07 10:36:55 +0300

    feat: add world interface

---
+++ Runtime/Worlds/IWorld.cs
@@ -0,0 +1,9 @@
+﻿namespace ECS
+{
+    public interface IWorld
+    {
+        public IEntities Entities { get; }
+        public IPools Pools { get; }
+        public IFilters Filters { get; }
+    }
+}
--- Runtime/Worlds/World.cs
+++ Runtime/Worlds/World.cs
@@ -1,6 +1,6 @@
 ﻿namespace ECS
 {
-    public sealed class World
+    public sealed class World : IWorld
     {
         public Entities EntitiesInternal { get; }
         public Pools PoolsInternal { get; }
@@ -8,19 +8,20 @@
 
         public IEntities Entities => EntitiesInternal;
         public IPools Pools => PoolsInternal;
+        public IFilters Filters => FiltersInternal;
 
         public World()
         {
             EntitiesInternal = new Entities();
             PoolsInternal = new Pools();
-            FiltersInternal = new Filters();
+            FiltersInternal = new Filters(this);
         }
 
         public World((int sparse, int dense) entitiesCapacity, int poolsCapacity, (int included, int excluded) filtersCapacity)
         {
             EntitiesInternal = new Entities(entitiesCapacity.sparse, entitiesCapacity.dense);
             PoolsInternal = new Pools(poolsCapacity);
-            FiltersInternal = new Filters(filtersCapacity.included, filtersCapacity.excluded);
+            FiltersInternal = new Filters(this, filtersCapacity.included, filtersCapacity.excluded);
         }
     }
 }
