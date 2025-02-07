f3a0700e0a029363dc8c53a75a526e22c5a0fb81
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-07 10:37:56 +0300

    test: add filter build test

--- Tests/Editor/Filters/Filters.cs
+++ Tests/Editor/Filters/Filters.cs
@@ -7,9 +7,9 @@ namespace ECS.Tests
         [Test]
         public void Constructor()
         {
-            var filters = new ECS.Filters();
-            var filters10x10 = new ECS.Filters(10, 10);
-            var filters_10x_10 = new ECS.Filters(-10, -10);
+            var filters = new ECS.Filters(null);
+            var filters10x10 = new ECS.Filters(null, 10, 10);
+            var filters_10x_10 = new ECS.Filters(null, -10, -10);
 
             Assert.AreEqual((0, 0), filters.Length);
             Assert.AreEqual((0, 0), filters10x10.Length);
@@ -17,9 +17,36 @@ namespace ECS.Tests
         }
 
         [Test]
+        public void Build()
+        {
+            var world = new World();
+            var entity1x0 = world.Entities.Create();
+            var entity2x0 = world.Entities.Create();
+            var filterInt = world.Filters.Create().Include<int>().Build();
+
+            world.Pools.Get<int>().Set(entity1x0, 10);
+            world.Pools.Get<string>().Set(entity1x0, "10");
+            world.FiltersInternal.Create(entity1x0, typeof(int));
+            world.FiltersInternal.Create(entity1x0, typeof(string));
+
+            Assert.AreEqual((1, 0), world.FiltersInternal.Length);
+            Assert.True(filterInt.Contains(entity1x0));
+            Assert.False(filterInt.Contains(entity2x0));
+
+            var filterIntString = world.Filters.Create(2, 2).Include<int, string>().Build();
+            var filter_String = world.Filters.Create(4, 4).Exclude<string>().Build();
+
+            Assert.AreEqual((2, 1), world.FiltersInternal.Length);
+            Assert.True(filterIntString.Contains(entity1x0));
+            Assert.False(filterIntString.Contains(entity2x0));
+            Assert.False(filter_String.Contains(entity1x0));
+            Assert.True(filter_String.Contains(entity2x0));
+        }
+
+        [Test]
         public void Include()
         {
-            var filters = new ECS.Filters();
+            var filters = new ECS.Filters(null);
             var filter = new ECS.Filter(2);
             var entity1x0 = new ECS.Entity(1, 0);
             var entity2x0 = new ECS.Entity(2, 0);
@@ -39,7 +66,7 @@ namespace ECS.Tests
         [Test]
         public void Exclude()
         {
-            var filters = new ECS.Filters();
+            var filters = new ECS.Filters(null);
             var filter = new ECS.Filter(2);
             var entity1x0 = new ECS.Entity(1, 0);
             var entity2x0 = new ECS.Entity(2, 0);
@@ -60,7 +87,7 @@ namespace ECS.Tests
         [Test]
         public void Create()
         {
-            var filters = new ECS.Filters();
+            var filters = new ECS.Filters(null);
             var filter = new ECS.Filter(1);
             var entity = new ECS.Entity();
             var entity1x0 = new ECS.Entity(1, 0);
@@ -81,7 +108,7 @@ namespace ECS.Tests
         [Test]
         public void Remove()
         {
-            var filters = new ECS.Filters();
+            var filters = new ECS.Filters(null);
             var filter = new ECS.Filter(1);
             var entity = new ECS.Entity();
             var entity1x0 = new ECS.Entity(1, 0);
