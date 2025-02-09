ba518c438c0de4fdd73495ee1d98efc19167dbd6
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-09 20:00:35 +0300

    feat: add filter and filters options

---
+++ Runtime/Options/OptionsFilter.cs
@@ -0,0 +1,29 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsFilter
+    {
+        public const int DefaultEntitiesCapacity = OptionsEntities.DefaultCapacity;
+        public const int DefaultCapacity = 16;
+
+        public readonly int EntitiesCapacity;
+        public readonly int Capacity;
+
+        public OptionsFilter(int entitiesCapacity, int capacity)
+        {
+            EntitiesCapacity = entitiesCapacity;
+            Capacity = capacity;
+        }
+
+        public static OptionsFilter Default()
+        {
+            return new OptionsFilter(DefaultEntitiesCapacity, DefaultCapacity);
+        }
+
+        public OptionsFilter Validate()
+        {
+            return new OptionsFilter(
+                EntitiesCapacity > 1 ? EntitiesCapacity : DefaultEntitiesCapacity,
+                Capacity > 0 ? Capacity : DefaultCapacity);
+        }
+    }
+}
---
+++ Runtime/Options/OptionsFilters.cs
@@ -0,0 +1,29 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsFilters
+    {
+        public const int DefaultCapacity = 32;
+        public const int DefaultFiltersCapacity = 8;
+
+        public readonly int Capacity;
+        public readonly int FiltersCapacity;
+
+        public OptionsFilters(int capacity, int filtersCapacity)
+        {
+            Capacity = capacity;
+            FiltersCapacity = filtersCapacity;
+        }
+
+        public static OptionsFilters Default()
+        {
+            return new OptionsFilters(DefaultCapacity, DefaultFiltersCapacity);
+        }
+
+        public OptionsFilters Validate()
+        {
+            return new OptionsFilters(
+                Capacity > 0 ? Capacity : DefaultCapacity,
+                FiltersCapacity > 0 ? FiltersCapacity : DefaultFiltersCapacity);
+        }
+    }
+}
