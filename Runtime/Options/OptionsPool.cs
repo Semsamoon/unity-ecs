d6d78d8ca859610dc117c8cf03aca9617ee84235
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-09 19:38:13 +0300

    feat: add pool and pools options

---
+++ Runtime/Options/OptionsPool.cs
@@ -0,0 +1,29 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsPool
+    {
+        public const int DefaultEntitiesCapacity = OptionsEntities.DefaultCapacity;
+        public const int DefaultCapacity = 16;
+
+        public readonly int EntitiesCapacity;
+        public readonly int Capacity;
+
+        public OptionsPool(int entitiesCapacity, int capacity)
+        {
+            EntitiesCapacity = entitiesCapacity;
+            Capacity = capacity;
+        }
+
+        public static OptionsPool Default()
+        {
+            return new OptionsPool(DefaultEntitiesCapacity, DefaultCapacity);
+        }
+
+        public OptionsPool Validate()
+        {
+            return new OptionsPool(
+                EntitiesCapacity > 1 ? EntitiesCapacity : DefaultEntitiesCapacity,
+                Capacity > 0 ? Capacity : DefaultCapacity);
+        }
+    }
+}
---
+++ Runtime/Options/OptionsPools.cs
@@ -0,0 +1,24 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsPools
+    {
+        public const int DefaultCapacity = 32;
+
+        public readonly int Capacity;
+
+        public OptionsPools(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public static OptionsPools Default()
+        {
+            return new OptionsPools(DefaultCapacity);
+        }
+
+        public OptionsPools Validate()
+        {
+            return new OptionsPools(Capacity > 0 ? Capacity : DefaultCapacity);
+        }
+    }
+}
