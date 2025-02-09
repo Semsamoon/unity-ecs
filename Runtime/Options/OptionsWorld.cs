d5429668cc0c8fdb43da353d875859588f2c9c7b
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-09 20:45:57 +0300

    feat: add world options

---
+++ Runtime/Options/OptionsWorld.cs
@@ -0,0 +1,65 @@
+﻿namespace ECS
+{
+    public struct OptionsWorld
+    {
+        public OptionsEntities Entities { get; private set; }
+        public OptionsPools Pools { get; private set; }
+        public OptionsPool Pool { get; private set; }
+        public OptionsFilters Filters { get; private set; }
+        public OptionsFilter Filter { get; private set; }
+        public OptionsSystems Systems { get; private set; }
+
+        public OptionsWorld With(OptionsEntities entities)
+        {
+            Entities = entities;
+            return this;
+        }
+
+        public OptionsWorld With(OptionsPools pools)
+        {
+            Pools = pools;
+            return this;
+        }
+
+        public OptionsWorld With(OptionsPool pool)
+        {
+            Pool = pool;
+            return this;
+        }
+
+        public OptionsWorld With(OptionsFilters filters)
+        {
+            Filters = filters;
+            return this;
+        }
+
+        public OptionsWorld With(OptionsFilter filter)
+        {
+            Filter = filter;
+            return this;
+        }
+
+        public OptionsWorld With(OptionsSystems systems)
+        {
+            Systems = systems;
+            return this;
+        }
+
+        public static OptionsWorld Default()
+        {
+            return new OptionsWorld()
+                .With(OptionsEntities.Default()).With(OptionsPools.Default()).With(OptionsPool.Default())
+                .With(OptionsFilters.Default()).With(OptionsFilter.Default()).With(OptionsSystems.Default());
+        }
+
+        public void Validate()
+        {
+            Entities = Entities.Validate();
+            Pools = Pools.Validate();
+            Pool = Pool.Validate();
+            Filters = Filters.Validate();
+            Filter = Filter.Validate();
+            Systems = Systems.Validate();
+        }
+    }
+}
