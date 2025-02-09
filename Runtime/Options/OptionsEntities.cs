2c8268c8683921d8cc75bcc10df9a3d98be207db
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-09 19:12:11 +0300

    feat: add entities options

---
+++ Runtime/Options/OptionsEntities.cs
@@ -0,0 +1,29 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsEntities
+    {
+        public const int DefaultCapacity = 64;
+        public const int DefaultComponentsCapacity = 8;
+
+        public readonly int Capacity;
+        public readonly int ComponentsCapacity;
+
+        public OptionsEntities(int capacity, int componentsCapacity)
+        {
+            Capacity = capacity;
+            ComponentsCapacity = componentsCapacity;
+        }
+
+        public static OptionsEntities Default()
+        {
+            return new OptionsEntities(DefaultCapacity, DefaultComponentsCapacity);
+        }
+
+        public OptionsEntities Validate()
+        {
+            return new OptionsEntities(
+                Capacity > 1 ? Capacity : DefaultCapacity,
+                ComponentsCapacity > 0 ? ComponentsCapacity : DefaultComponentsCapacity);
+        }
+    }
+}
