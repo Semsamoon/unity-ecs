12b0cacc9b2d957d06d3dc3487336c22782de697
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-09 20:21:38 +0300

    feat: add systems options

---
+++ Runtime/Options/OptionsSystems.cs
@@ -0,0 +1,24 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsSystems
+    {
+        public const int DefaultCapacity = 16;
+
+        public readonly int Capacity;
+
+        public OptionsSystems(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public static OptionsSystems Default()
+        {
+            return new OptionsSystems(DefaultCapacity);
+        }
+
+        public OptionsSystems Validate()
+        {
+            return new OptionsSystems(Capacity > 0 ? Capacity : DefaultCapacity);
+        }
+    }
+}
