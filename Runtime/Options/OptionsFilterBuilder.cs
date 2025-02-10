3070d81639385b31c7a117e2cd96b6f7d4b49653
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-10 10:51:04 +0300

    feat: add filter builder options

---
+++ Runtime/Options/OptionsFilterBuilder.cs
@@ -0,0 +1,29 @@
+﻿namespace ECS
+{
+    public readonly struct OptionsFilterBuilder
+    {
+        public const int DefaultIncludedCapacity = 4;
+        public const int DefaultExcludedCapacity = 4;
+
+        public readonly int IncludedCapacity;
+        public readonly int ExcludedCapacity;
+
+        public OptionsFilterBuilder(int includedCapacity, int excludedCapacity)
+        {
+            IncludedCapacity = includedCapacity;
+            ExcludedCapacity = excludedCapacity;
+        }
+
+        public static OptionsFilterBuilder Default()
+        {
+            return new OptionsFilterBuilder(DefaultIncludedCapacity, DefaultExcludedCapacity);
+        }
+
+        public OptionsFilterBuilder Validate()
+        {
+            return new OptionsFilterBuilder(
+                IncludedCapacity > 0 ? IncludedCapacity : DefaultIncludedCapacity,
+                ExcludedCapacity > 0 ? ExcludedCapacity : DefaultExcludedCapacity);
+        }
+    }
+}
