ba35dab1c8c0750ac6ba8e5ebf396aaec60be1c2
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-02-12 10:35:45 +0300

    feat: add logger

---
+++ Runtime/Debug/Logger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ECS
+{
+    public static class Logger
+    {
+        public static void Warning(string message)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning($"<ECS <color=yellow>WARNING</color>> {message}");
+#else
+            Debug.LogWarning($"<ECS WARNING> {message}");
+#endif
+        }
+
+        public static void Error(string message)
+        {
+#if UNITY_EDITOR
+            Debug.LogError($"<ECS <color=red>ERROR</color>> {message}");
+#else
+            Debug.LogError($"<ECS ERROR> {message}");
+#endif
+        }
+    }
+}
